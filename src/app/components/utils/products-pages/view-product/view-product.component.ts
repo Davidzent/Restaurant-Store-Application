3123bc156ca97f6677d1e937600812064e78de23
Davidgin <davidgin641@gmail.com>
2022-02-22 08:40:05 -0800

    Templates: add a view-product for a more ditailed view of a product add a view-purchase for a more ditailed view of a purchase
    Signed-off-by: Davidgin <davidgin641@gmail.com>

---
+++ src/app/components/utils/products-pages/view-product/view-product.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/interfaces/Iproduct';
+
+@Component({
+  selector: 'app-view-product',
+  templateUrl: './view-product.component.html',
+  styleUrls: ['./view-product.component.css']
+})
+export class ViewProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input() input:IProduct = {
+    product_id: 0,
+    price: 0,
+    description: '',
+    seller: {
+      user_id:0,   //unique id
+      fname:"",
+      lname:"",
+      email:""   //don't know if we should display this
+    },
+    status: 0,
+    typeid: 0
+  };
+
+}
