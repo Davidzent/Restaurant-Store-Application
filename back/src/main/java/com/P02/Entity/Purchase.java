adc0c77d274b4903321ede8f2a5f80e36d692aca
mistyhippo <32620699+mistyhippo@users.noreply.github.com>
2022-02-24 08:09:02 -0800

    updates
    add dependencies to build.gradle
    create testproduct repository
    add bootstrap js

---
+++ back/src/main/java/com/P02/Entity/Purchase.java
@@ -0,0 +1,19 @@
+package com.P02.Entity;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+/*
+@Entity
+@Table(name="purchase")
+@Data*/
+/*
+public class Purchase {
+
+    private int purchase_id;
+    Product product;
+
+}
+*/
